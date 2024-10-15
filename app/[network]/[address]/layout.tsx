e5c5f452192518c09645895bd154f9a1ba107192
aliyan404 <1434451678@qq.com>
2024-10-15 16:46:42 +0800

    fix: style

--- app/[network]/[address]/layout.tsx
+++ app/[network]/[address]/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <Providers>
       <div className="flex flex-col h-screen">
         <Header />
-        <div className="flex-1 overflow-auto">{children}</div>
+        {children}
       </div>
     </Providers>
   )
